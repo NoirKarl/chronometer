c64f0f8fa974d0284d8020d9445424f2fc4203d9
Noirkarl <carloscm4231@gmail.com>
2024-04-06 10:12:49 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
-let tiempoRef = Date.now()
-let cronometrar = false
-let acumulado = 0
+const generarPaleta = document.getElementById('palleteContainer');
+const colorValues = ['1','2','3','4','6','7','8','9','A','B','C','D','E','F'];
+const PALLETE_SIZE = 5;
 
-    function iniciar() {
-        cronometrar = true
+
+createPallete = () =>{
+    palleteContainer.innerHTML = '';
+    for(let i = 0; i< PALLETE_SIZE; i++){
+        const palleteElement = document.createElement('div');
+        palleteElement.classList.add('palleteItem');
+        palleteContainer.appendChild(palleteElement);
     }
-
-        function pausar() {
-            cronometrar = false
-        }
-
-    function reiniciar() {
-        acumulado = 0
-    }
-
-    setInterval(() => {
-        let tiempo = document.getElementById("tiempo")
-        if (cronometrar) {
-            acumulado += Date.now() - tiempoRef
-        }
-        tiempoRef = Date.now()
-        tiempo.innerHTML = formatearMS(acumulado)
-    }, 1000 / 60);
-
-    function formatearMS(tiempo_ms) {
-        let MS = tiempo_ms % 1000
-        let St = Math.floor(((tiempo_ms - MS) / 1000))
-        let S = St%60
-        let M = Math.floor((St / 60) % 60)
-        let H = Math.floor((St/60 / 60))
-        Number.prototype.ceros = function (n) {
-        return (this + "").padStart(n, 0)
-        }
-
-        return H.ceros(2) + ":" + M.ceros(2) + ":" + S.ceros(2)
-        + "." + MS.ceros(3)
+    updatePallete();
 }
+
+const colorize = (element) =>{
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        const randomElement = colorValues[Math.floor(Math.random() * colorValues.length)];
+        color += randomElement;
+    };
+    element.style.backgroundColor = color;
+    element.innerHTML = `<span class='colorHex'>${color}</span>`;  
+}
+
+const updatePallete = () =>{
+    for (let i = 0; i < palleteContainer.children.length; i++) {
+        colorize(palleteContainer.children[i])
+    }
+}
+
+createPallete();
